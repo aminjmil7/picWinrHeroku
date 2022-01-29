00d48e91f44b2be022e4dd89e6408321fb22db5f
amin jmil <aminjmil7@gmail.com>
2022-01-29 17:03:54 +0100

    Deploy to Heroku

--- src/main/webapp/app/entities/welcome/social-instagram/social-instagram.service.ts
+++ src/main/webapp/app/entities/welcome/social-instagram/social-instagram.service.ts
@@ -7,16 +7,12 @@ export class InstagramService {
   constructor(protected http: HttpClient) {}
 
   getAccessToken(client_id: string, client_secret: string, redirect_uri: string, code: string): Observable<any> {
-    return this.http.post<any>(
-      'https://api.instagram.com/oauth/access_token?client_id=' +
-        client_id +
-        '&client_secret=ea04816ebaa1690167fdb3d9c39580c5' +
-        '&grant_type=authorization_code' +
-        '&redirect_uri=' +
-        redirect_uri +
-        '&code=' +
-        code,
-      { observe: 'response' }
-    );
+    return this.http.post<any>('https://api.instagram.com/oauth/access_token', {
+      client_id: client_id,
+      client_secret: 'ea04816ebaa1690167fdb3d9c39580c5',
+      code: code,
+      grant_type: 'authorization_code',
+      redirect_uri: 'redirect_uri',
+    });
   }
 }
